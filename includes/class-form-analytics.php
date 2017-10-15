c2cca2934f03861601c1590806345ef2edc04170
Nick Breslin <nickbreslin@gmail.com>
2017-10-15 14:15:32 -0700

    Updating plugin_name

---
+++ includes/class-form-analytics.php
@@ -0,0 +1,170 @@
+<?php
+/**
+ * The file that defines the core plugin class
+ *
+ * A class definition that includes attributes and functions used across both the
+ * public-facing side of the site and the admin area.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Form_Analytics
+ * @subpackage Form_Analytics/includes
+ */
+
+/**
+ * The core plugin class.
+ *
+ * This is used to define internationalization, admin-specific hooks, and
+ * public-facing site hooks.
+ *
+ * Also maintains the unique identifier of this plugin as well as the current
+ * version of the plugin.
+ *
+ * @since      1.0.0
+ * @package    Form_Analytics
+ * @subpackage Form_Analytics/includes
+ * @author     Your Name <email@example.com>
+ */
+class Form_Analytics {
+
+	/**
+	 * The loader that's responsible for maintaining and registering all hooks that power
+	 * the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      Form_Analytics_Loader    $loader    Maintains and registers all hooks for the plugin.
+	 */
+	protected $loader;
+
+	/**
+	 * The unique identifier of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
+	 */
+	protected $plugin_name;
+
+	/**
+	 * The current version of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $version    The current version of the plugin.
+	 */
+	protected $version;
+
+	/**
+	 * Define the core functionality of the plugin.
+	 *
+	 * Set the plugin name and the plugin version that can be used throughout the plugin.
+	 * Load the dependencies, define the locale, and set the hooks for the admin area and
+	 * the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function __construct() {
+		if ( defined( 'FORM_ANALYTICS_VERSION' ) ) {
+			$this->version = FORM_ANALYTICS_VERSION;
+		} else {
+			$this->version = '1.0.0';
+		}
+		$this->plugin_name = 'form-analytics';
+
+		$this->load_dependencies();
+		$this->define_public_hooks();
+
+	}
+
+	/**
+	 * Load the required dependencies for this plugin.
+	 *
+	 * Include the following files that make up the plugin:
+	 *
+	 * - Form_Analytics_Loader. Orchestrates the hooks of the plugin.
+	 * - Form_Analytics_Public. Defines all hooks for the public side of the site.
+	 *
+	 * Create an instance of the loader which will be used to register the hooks
+	 * with WordPress.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function load_dependencies() {
+
+		/**
+		 * The class responsible for orchestrating the actions and filters of the
+		 * core plugin.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-form-analytics-loader.php';
+
+		/**
+		 * The class responsible for defining all actions that occur in the public-facing
+		 * side of the site.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-form-analytics-public.php';
+
+		$this->loader = new Form_Analytics_Loader();
+
+	}
+
+
+	/**
+	 * Register all of the hooks related to the public-facing functionality
+	 * of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function define_public_hooks() {
+
+		$plugin_public = new Form_Analytics_Public( $this->get_plugin_name(), $this->get_version() );
+
+		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
+		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+
+	}
+
+	/**
+	 * Run the loader to execute all of the hooks with WordPress.
+	 *
+	 * @since    1.0.0
+	 */
+	public function run() {
+		$this->loader->run();
+	}
+
+	/**
+	 * The name of the plugin used to uniquely identify it within the context of
+	 * WordPress and to define internationalization functionality.
+	 *
+	 * @since     1.0.0
+	 * @return    string    The name of the plugin.
+	 */
+	public function get_plugin_name() {
+		return $this->plugin_name;
+	}
+
+	/**
+	 * The reference to the class that orchestrates the hooks with the plugin.
+	 *
+	 * @since     1.0.0
+	 * @return    Form_Analytics_Loader    Orchestrates the hooks of the plugin.
+	 */
+	public function get_loader() {
+		return $this->loader;
+	}
+
+	/**
+	 * Retrieve the version number of the plugin.
+	 *
+	 * @since     1.0.0
+	 * @return    string    The version number of the plugin.
+	 */
+	public function get_version() {
+		return $this->version;
+	}
+
+}
---
+++ public/js/form-analytics-public.js
@@ -0,0 +1,446 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = ( window.console = window.console || {} );
+
+    while ( length-- ) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if ( !console[method] ) {
+            console[method] = noop;
+        }
+    }
+}());
+
+//
+// Compatability with WordPress
+//
+function truthy( value ) {
+        "use strict";
+ 
+        value = typeof value !== "undefined" ? value : "";
+
+        var result = false;
+
+        switch( value ) {
+
+        	case "1":
+        	case 1:
+        	case true:
+        	case "true":
+        		result = true;
+        		break;
+
+        	default:
+        		break;
+        }
+
+        return result;
+}
+
+function _log( s ) {
+	"use strict";
+	if ( ( typeof boosh_form_config !== "undefined" ) &&
+		truthy( boosh_form_config.debug ) ) {
+		console.log( "[Boosh]: " + s );
+	}
+}
+
+function _err(s) {
+	"use strict";
+
+	if ( ( typeof boosh_form_config !== "undefined" ) &&
+		truthy( boosh_form_config.debug ) ) {
+		console.error( "[Boosh]: " + s );
+	}
+}
+
+var BooshForm = {
+
+	dom  : null,
+	name : "",
+	type : "",
+	status : 0,
+
+	events : {
+		visibilityPartial  : false,
+		visibilityComplete : false,
+		fieldFocus         : false,
+		submitAttempt      : false,
+		submitSuccess      : false
+	},
+
+	position : {
+		top    : null,
+		bottom : null
+	},
+
+	setName : function( slug, id, name, role, key ) {
+		"use strict";
+
+		slug = slug === undefined || slug === null ? "" : slug + "_";
+		id   = id   === undefined || id   === null ? "" : id   + "_";
+		name = name === undefined || name === null ? "" : name + "_";
+		role = role === undefined || role === null ? "" : role + "_";
+		key  = key  === undefined || key  === null ? "" : key;
+
+		var formName = slug + id + name + role + key;
+
+		if ( formName === "" ) {
+			_err('Form Name cannot be set');
+			return false;
+		}
+
+		this.name = formName;
+	},
+
+	record : function ( type ) {
+		"use strict";
+
+		switch( type ) {
+			case 'fieldFocus':
+				if ( this.events.fieldFocus === false ) {
+					this.events.fieldFocus = true;
+					this.trigger( type );
+				}
+				break;
+
+			case 'visibilityPartial':
+				if ( this.events.visibilityPartial === false ) {
+					this.events.visibilityPartial = true;
+					this.trigger( type );
+				}
+				break;
+
+			case 'visibilityComplete':
+				if ( this.events.visibilityComplete === false ) {
+					this.events.visibilityComplete = true;
+					this.trigger( type );
+				}
+				break;
+
+			case 'submitAttempt':
+				// Can be submitted more than once.
+				this.events.submitAttempt = true;
+				this.status = 1;
+				this.trigger( type );
+				break;
+
+			case 'submitSuccess':
+				if ( this.events.submitSuccess === false ) {
+					this.events.submitSuccess = true;
+					this.trigger( type );
+				}
+				break;
+
+			default:
+				_log('Unknown event type: ' + type);
+				return false;
+		}
+	},
+
+	trigger : function ( type ) {
+		"use strict";
+
+		_log( this.name + ': Event ' + type + ' firing.' );
+
+		var booshForm = this;
+
+	    var params = {
+	        hitType       : 'event',
+	        eventCategory : 'Booshlin Form Events',
+	        eventAction   : type,
+	        eventLabel    : this.name,
+	        hitCallback : function() {
+		    	_log( booshForm.name + ': Event ' + type + ' successful.' );
+		    }
+	    };
+
+	    var _ga = boosh.formManager.getGA();
+	    _ga( 'send', params );
+	},
+
+	findType : function () {
+		"use strict";
+
+		var classes = this.dom.className.split(' ');
+		if ( classes.indexOf('wpcf7-form') !== -1 ) {
+			return 'wpcf7-form';
+		}
+
+		return "";
+	},
+
+	setType : function ( type ) {
+		"use strict";
+
+		this.type = type;
+	},
+
+	cumulativeOffset: function(element) {
+		'use strict';
+		
+	    var top = 0, left = 0;
+
+	    do {
+	        top    += element.offsetTop  || 0;
+	        left   += element.offsetLeft || 0;
+	        element = element.offsetParent;
+	    } while ( element );
+
+	    return {
+	        top  : top,
+	        left : left
+	    };
+	}
+};
+
+var boosh = boosh || {};
+
+boosh.formManager = boosh.formManager || {
+
+	forms  : [],
+	isInit : false,
+
+	getGA : function () {
+
+		if ( typeof window.GoogleAnalyticsObject === "undefined" ) {
+	 		_err( 'No GoogleAnalyticsObject' );
+	 		return false;
+	 	}
+
+	 	var tracker = window.GoogleAnalyticsObject;
+
+	 	if ( typeof window[tracker] === "undefined" ) {
+	 		_err( 'No GoogleAnalyticsObject' );
+	 		return false;
+	 	}
+
+	 	return window[tracker];
+	},
+
+	init : function() {
+		'use strict';
+
+		_log( 'Initializing...' );
+
+		if ( truthy(this.isInit) ) {
+			return false;
+		}
+
+		var _ga = this.getGA();
+
+		if( _ga === false ) {
+			_err("No Google Analytics");
+			return false;
+		}
+
+		_ga( this.run() );
+
+		this.isInit = true;
+	},
+
+	run : function () {
+
+		_log( 'Running...' );
+
+		var forms = [];
+
+		forms = this.detect( forms );
+		forms = this.exclude( forms );
+
+		this.validate( forms );
+
+		this.bind();
+		this.watch();
+		this.visibilityCheck();
+	},
+
+	detect : function() {
+		'use strict';
+
+		var forms = document.forms;
+
+		_log( 'Detected Forms: ' + forms.length );
+
+		return forms;
+	},
+
+	exclude: function( forms ) {
+		'use strict';
+
+		// Exclude a form by the id attribute value
+		var excludeById = [
+			'adminbarsearch'
+		];
+
+		// Exclude a form by the role attribute value
+		var excludeByRole = [
+			'search'
+		];
+
+		var filteredForms = [];
+
+
+		for ( var i = 0 ; i < forms.length ; i++ ) {
+
+			var form = forms[i];
+			var id   = form.getAttribute('id');
+			var role = form.getAttribute('role');
+
+			if ( excludeById.indexOf( id ) === 0 ) {
+				_log( 'Excluding ID: ' + id );
+				continue;
+			}
+
+			if ( excludeByRole.indexOf( role ) === 0 ) {
+				_log( 'Excluding Role: ' + role );
+				continue;
+			}
+
+			_log( 'Accepting: ' + id + ":" + role );
+			filteredForms.push( form );
+		}
+
+		_log( 'Filtered Forms: ' + filteredForms.length );
+
+		return filteredForms;
+	},
+
+
+	validate :  function( forms ) {
+
+		for ( var i = 0 ; i < forms.length ; i++ ) {
+
+			var form = forms[i];
+			var booshForm = jQuery.extend( true, {}, BooshForm );
+
+			booshForm.dom = form;
+
+			var position = booshForm.cumulativeOffset(form);
+
+	 		booshForm.position.top    = position.top;
+	 		booshForm.position.bottom = jQuery(form).height() + position.top;
+
+	 		
+	 		var id   = form.getAttribute( 'id' );
+			var role = form.getAttribute( 'role' );
+			var name = form.getAttribute( 'name' );
+
+			var slug = typeof boosh_form_config.slug !== "undefined" ? boosh_form_config.slug : "";
+
+	 		booshForm.setName(slug, id, role, name, i);
+
+	 		var type = booshForm.findType();
+	 		booshForm.setType( type );
+
+	 		_log( booshForm.name + ' validated' );
+			this.forms.push( booshForm );
+		}
+	},
+
+	bind : function () {
+		"use strict";
+
+		jQuery( document ).ajaxComplete( function ( e, r, s ) {
+
+				e.preventDefault();
+
+				if(s.type != "POST") {
+					_log('not post');
+					return;
+				}
+
+				if(s.url.indexOf("/feedback") == -1) {
+					_log('not feedback');
+					return;
+				}
+
+				var form = e.currentTarget.activeElement.parentElement.parentElement;
+
+				jQuery.each( boosh.formManager.forms, function( index, booshForm ) {
+	
+					if ( form == booshForm.dom || booshForm.status == 1 ) {
+
+						var classes = booshForm.dom.className.split(' ');
+
+						if ( classes.indexOf( 'sent' ) !== -1 ) {
+							_log('sent');
+							booshForm.record( 'submitSuccess' );
+						}
+
+						if ( classes.indexOf( 'invalid' )  !== -1 ) {
+							_log('invalid');
+						//	booshForm.record( 'submitFailure' );
+						}
+
+						booshForm.status = 0;
+					}
+				});
+		});
+
+		jQuery.each( this.forms, function( index, booshForm ) {
+
+			jQuery( ':input', booshForm.dom ).not( ':input[type=submit]' ).focus( function () {
+				booshForm.record( 'fieldFocus' );
+			});
+			
+			if(booshForm.type == "wpcf7-form") {
+				jQuery( booshForm.dom ).submit( function (e) {
+					e.preventDefault();
+					booshForm.record( 'submitAttempt' );
+				});
+			}
+		});
+
+		
+	},
+
+	watch : function () {
+		'use strict';
+
+		// TODO - set time delay so it doesn't trigger too often
+		jQuery( document ).scroll( function () {
+			boosh.formManager.visibilityCheck();
+	 	});
+	},
+
+	visibilityCheck : function () {
+		'use strict';
+
+ 		var bottom = jQuery( window ).height() + jQuery( document ).scrollTop();
+
+		jQuery.each( boosh.formManager.forms, function( index, booshForm ) {
+
+	 		if ( booshForm.position.bottom == 0 ) {
+	 			_log( booshForm.name + ' position not set' );
+	 			return;
+	 		}
+
+	 		if ( bottom > booshForm.position.top ) {
+	 			booshForm.record( 'visibilityPartial' );
+	 		}
+
+	 		if ( bottom > booshForm.position.bottom ) {
+	 			booshForm.record( 'visibilityComplete' );
+	 		}
+		});
+	}
+};
+
+//
+// 
+//
+jQuery( function() {
+	_log( 'DOM ready' );
+ 	boosh.formManager.init();
+});
